2b4e74a724aea1f4ad69091211b5c34ad91398f3
王亚正 <wangyz@sinyoo.net>
2017-11-04 15:39:52 +0800

    设置好服务及组件，配置好路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AmexioWidgetModule, CommonHttpService } from 'amexio-ng-extensions';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { QueryComponent } from './query/query.component';
+import { StatisticComponent } from './statistic/statistic.component';
+import { ThemeService } from './theme.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WorkspaceComponent,
+    QueryComponent,
+    StatisticComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmexioWidgetModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'workspace',
+            children: [
+                {path : '', component: WorkspaceComponent},
+                { path: 'query', component: QueryComponent},
+                { path: 'statistic', component: StatisticComponent}
+            ]}
+      ])
   ],
-  providers: [],
+  providers: [CommonHttpService, ThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
